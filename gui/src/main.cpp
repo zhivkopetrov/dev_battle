1f8705214a090d7b7e3a40988cad5fea5841c6ce
zhivko.petrov <zhivkopetrov90@gmail.com>
2020-07-05 18:39:57 +0300

    Initial commit for dev_battle_server & dev_battle_gui binaries

---
+++ gui/src/main.cpp
@@ -0,0 +1,17 @@
+// C system headers
+
+// C++ system headers
+#include <cstdlib>
+
+// Other libraries headers
+
+// Own components headers
+#include "utils/Log.h"
+
+int main() {
+  LOGC("Hello from gui main");
+
+  return 0;
+}
+
+
