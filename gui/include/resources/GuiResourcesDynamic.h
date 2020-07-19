3af99f81ece386c427e8d4168d47606a5ee31f50
zhivko.petrov <zhivkopetrov90@gmail.com>
2020-07-19 23:00:38 +0300

    Add resource assets for archer, knight, spearman

---
+++ gui/include/resources/GuiResourcesDynamic.h
@@ -0,0 +1,18 @@
+/* This file is auto-generated. Do not modify it. */
+
+/* Resource Builder Tool version 1.0 */
+/* Author: Zhivko Petrov */
+
+#ifndef GUI_INCLUDE_RESOURCES__HOME_ZHIVKO_WORKSPACE_DEV_BATTLE_GUI_INCLUDE_RESOURCES_GUIRESOURCES_H_DYNAMIC
+#define GUI_INCLUDE_RESOURCES__HOME_ZHIVKO_WORKSPACE_DEV_BATTLE_GUI_INCLUDE_RESOURCES_GUIRESOURCES_H_DYNAMIC
+
+#include <cstdint>
+
+namespace GuiResourcesDynamic
+{
+    enum ResourceTags : uint64_t
+    {
+    }; /* enum ResourceTags */
+} /* namespace GuiResourcesDynamic */
+
+#endif /* GUI_INCLUDE_RESOURCES__HOME_ZHIVKO_WORKSPACE_DEV_BATTLE_GUI_INCLUDE_RESOURCES_GUIRESOURCES_H_DYNAMIC */
