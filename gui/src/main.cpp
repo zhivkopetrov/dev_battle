6a4d2a17e61bf0f8897b0ba29398da74a4850f31
zhivko.petrov <zhivko.petrov@ocado.com>
2022-01-07 00:01:19 +0200

    feat: utilized default engine config generation

--- gui/src/main.cpp
+++ gui/src/main.cpp
@@ -17,27 +17,9 @@
 
 namespace {
 //TODO parse the params from config
-constexpr auto windowDisplayMode = WindowDisplayMode::FULL_SCREEN;
-constexpr auto windowBorderMode = WindowBorderMode::BORDERLESS;
-constexpr auto MONITOR_WIDTH = 1920;
-constexpr auto MONITOR_HEIGHT = 1080;
-constexpr auto MAX_FRAME_RATE = 75;
-constexpr auto PROJECT_NAME = "gui";
-constexpr auto LOADING_SCREEN_RELATIVE_TO_ROOT_PATH =
+constexpr auto PROJECT_FOLDER_NAME = "gui";
+constexpr auto LOADING_SCREEN_RESOURCES_PATH =
     "gui/include/resources/p/loading_screen/";
-constexpr auto HARDWARE_CONCURRENCY_HINT = 0;
-constexpr auto MAX_RESOURCE_LOADING_THREADS = HARDWARE_CONCURRENCY_HINT;
-
-constexpr auto MAX_RUNTIME_TEXTS = 400;
-constexpr auto MAX_RUNTIME_SPRITE_BUFFERS = 50;
-constexpr auto MAX_RUNTIME_IMAGES = 200;
-constexpr auto MAX_RUNTIME_WIDGETS =  MAX_RUNTIME_TEXTS +
-    MAX_RUNTIME_SPRITE_BUFFERS + MAX_RUNTIME_IMAGES;
-constexpr auto MAX_RUNTIME_RENDERER_COMMANDS =
-    MAX_RUNTIME_WIDGETS * 2;
-//65k bytes
-constexpr auto MAX_RENDERER_BACK_BUFFER_DATA_SIZE =
-    std::numeric_limits<uint16_t>::max();
 
 //game field tiles
 constexpr auto TILE_WIDTH = 78;
@@ -57,49 +39,9 @@ constexpr auto GAME_FIELD_HEIGHT =
 }
 
 static EngineConfig populateEngineConfig() {
-  EngineConfig cfg;
-
-  cfg.maxFrameRate = MAX_FRAME_RATE;
+  auto cfg = getDefaultEngineConfig(
+      PROJECT_FOLDER_NAME, LOADING_SCREEN_RESOURCES_PATH);
   cfg.debugConsoleRsrcId = GuiResources::VINQUE_RG;
-
-  cfg.managerHandlerCfg.drawMgrBaseCfg.rendererConfig.
-    maxRuntimeRendererCommands = MAX_RUNTIME_RENDERER_COMMANDS;
-  cfg.managerHandlerCfg.drawMgrBaseCfg.rendererConfig.maxRuntimeWidgets =
-      MAX_RUNTIME_WIDGETS;
-  cfg.managerHandlerCfg.drawMgrBaseCfg.rendererConfig.
-    maxRendererBackBufferDataSize = MAX_RENDERER_BACK_BUFFER_DATA_SIZE;
-
-  cfg.managerHandlerCfg.drawMgrBaseCfg.monitorWidth = MONITOR_WIDTH;
-  cfg.managerHandlerCfg.drawMgrBaseCfg.monitorHeight = MONITOR_HEIGHT;
-  cfg.managerHandlerCfg.drawMgrBaseCfg.windowDisplayMode = windowDisplayMode;
-  cfg.managerHandlerCfg.drawMgrBaseCfg.windowBorderMode = windowBorderMode;
-
-  cfg.managerHandlerCfg.sdlContainersCfg.maxResourceLoadingThreads =
-      MAX_RESOURCE_LOADING_THREADS;
-  cfg.managerHandlerCfg.sdlContainersCfg.maxRuntimeSpriteBuffers =
-      MAX_RUNTIME_SPRITE_BUFFERS;
-  cfg.managerHandlerCfg.sdlContainersCfg.maxRuntimeTexts =
-      MAX_RUNTIME_TEXTS;
-  cfg.managerHandlerCfg.sdlContainersCfg.resourcesBinLocation =
-      FileSystemUtils::getBuildDirectory();
-  cfg.managerHandlerCfg.sdlContainersCfg.resourcesBinLocation.append(
-      PROJECT_NAME).append("/").append(
-          ResourceFileHeader::getResourcesBinFolderName()).append("/");
-
-  cfg.managerHandlerCfg.sdlContainersCfg.loadingScreenCfg.loadingScreenUsage =
-      LoadingScreenUsage::ENABLED;
-  cfg.managerHandlerCfg.sdlContainersCfg.loadingScreenCfg.monitorWidth =
-      MONITOR_WIDTH;
-  cfg.managerHandlerCfg.sdlContainersCfg.loadingScreenCfg.monitorHeight =
-      MONITOR_HEIGHT;
-  const std::string loadingScreenFolderPath =
-     FileSystemUtils::getRootDirectory() + LOADING_SCREEN_RELATIVE_TO_ROOT_PATH;
-  cfg.managerHandlerCfg.sdlContainersCfg.loadingScreenCfg.backgroundImagePath =
-      loadingScreenFolderPath + "background.png";
-  cfg.managerHandlerCfg.sdlContainersCfg.loadingScreenCfg.
-    progressBarOnImagePath = loadingScreenFolderPath + "progressOn.png";
-  cfg.managerHandlerCfg.sdlContainersCfg.loadingScreenCfg.
-    progressBarOffImagePath = loadingScreenFolderPath + "progressOff.png";
 
   return cfg;
 }
